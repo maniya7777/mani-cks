86d3cd87e2f9450d9a2add096a97bf3b5133866b
maniya7777 <89803755+maniya7777@users.noreply.github.com>
2026-06-02 22:04:31 +0530

    Update question12.sh
    adding error after kubectl apply cmd

--- question12.sh
+++ question12.sh
@@ -19,6 +19,14 @@ The Deployment manifest file can be found at:
 
 ~/nginx-unprivileged.yaml
 
+cat <<'EOF'
+Warning: would violate PodSecurity "restricted:latest":
+allowPrivilegeEscalation != false (container "nginx" must set securityContext.allowPrivilegeEscalation=false),
+unrestricted capabilities (container "nginx" must set securityContext.capabilities.drop=["ALL"]),
+runAsNonRoot != true (pod or container "nginx" must set securityContext.runAsNonRoot=true),
+seccompProfile (pod or container "nginx" must set securityContext.seccompProfile.type to "RuntimeDefault" or "Localhost")
+EOF
+
 QUESTION
 
 echo
