2c57e584f15616544e9b72ccd032974671b85688
maniya7777 <89803755+maniya7777@users.noreply.github.com>
2026-06-12 00:42:57 +0530

    Update question04.sh
    correct question04

--- question04.sh
+++ question04.sh
@@ -8,7 +8,9 @@ echo
 echo "A misbehaving Pod poses a security threat to the system."
 echo
 echo "Task"
-echo "A Pod belonging to the ollama application is abnormal — it is directly accessing system memory by reading from the sensitive file /dev/mem."
+echo "A suspicious Pod in the cluster has been flagged for abnormal behavior."
+echo "A Pod belonging to the neuron application is attempting to access the sensitive host device"
+echo "This indicates a potential security threat and must be stopped."
 echo
 echo "First, identify the misbehaving Pod accessing /dev/mem."
 echo "Next, scale the Deployment of the misbehaving Pod to zero replicas."
@@ -17,12 +19,3 @@ echo "==============================================="
 
 echo
 echo "Environment setup completed."
-echo
-echo "Suggested approach:"
-echo "1. Gain root access."
-echo "2. Check the deployments."
-echo "3. Create a Falco rule to detect access to /dev/mem."
-echo "4. Monitor for 30 seconds and save the alerts to a file."
-echo "5. Review the Falco logs."
-echo "6. Identify the offending container using crictl or Docker commands."
-echo "7. Scale the corresponding Deployment to zero replicas."
