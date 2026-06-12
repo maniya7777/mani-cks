75d60404a5eff44626a3fc975df181d9085ea35d
maniya7777 <89803755+maniya7777@users.noreply.github.com>
2026-06-12 14:48:25 +0530

    Update question08.sh
    q08

--- question08.sh
+++ question08.sh
@@ -9,58 +9,31 @@ echo
 
 cat <<'QUESTION'
 
-Context
-You must expose a web application using HTTPS routing.
+There is an existing web application running in the production namespace behind a Service named web-service.
 
-Task
-Create an Ingress resource named web in the prod02 namespace and configure it as follows:
+Your goal is to publish this application externally using an Ingress with HTTPS.
 
-- Route traffic for the host web.k8sng.local and all paths to the existing web Service.
-- Enable TLS termination using the existing web-cert Secret.
-- Redirect HTTP requests to HTTPS.
+Create an Ingress resource called web-ingress in the production namespace with these requirements:
 
-PS:
-You can test the Ingress configuration using the following command:
+Accept traffic for the hostname web.k8s.local
+Forward all paths (/) to the existing web Service
+Terminate TLS using the existing Secret web-ingress-tls
+Ensure plain HTTP requests are automatically redirected to HTTPS
 
-[candidate@cks000032] $ curl -Lk https://web.k8sng.local
+For cillium use below doc:
+
+[https://docs.cilium.io/en/latest/network/servicemesh/ingress/]
+
+Validate using:
+
+# NGINX
+curl -Lk https://web.k8s.local:32000
+
+# Cilium
+curl -Lk https://web.k8s.local:32001
 
 QUESTION
 
-echo
-echo "============================================================="
-echo " Creating Killercoda Environment"
-echo "============================================================="
-echo
-
-kubectl create namespace prod02 >/dev/null 2>&1
-
-kubectl create deployment web \
-  --image=nginx \
-  -n prod02 >/dev/null 2>&1
-
-kubectl expose deployment web \
-  --port=80 \
-  --target-port=80 \
-  --name=web \
-  -n prod02 >/dev/null 2>&1
-
-openssl req -x509 -nodes -days 365 \
-  -newkey rsa:2048 \
-  -keyout /tmp/web.key \
-  -out /tmp/web.crt \
-  -subj "/CN=web.k8sng.local" >/dev/null 2>&1
-
-kubectl create secret tls web-cert \
-  --cert=/tmp/web.crt \
-  --key=/tmp/web.key \
-  -n prod02 >/dev/null 2>&1
-
-echo
-echo "[OK] Namespace created        : prod02"
-echo "[OK] Deployment created       : web"
-echo "[OK] Service created          : web"
-echo "[OK] TLS Secret created       : web-cert"
-echo
 
 echo "============================================================="
 echo " Ready for execution"
